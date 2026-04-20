79b0c2de0189194081def55e81ece78376a5b791
danielace1 <danielsudharsan1805@gmail.com>
2026-04-20 16:44:29 +0530

    feat: create class

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
 import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -10,13 +9,16 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
+import { BookOpen, GraduationCap, Home } from "lucide-react";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
-import Dashboard from "./pages/Dashboard";
-import { BookOpen, Home } from "lucide-react";
+
 import { Layout } from "./components/refine-ui/layout/layout";
+import Dashboard from "./pages/Dashboard";
 import SubjectsList from "./pages/subjects/SubjectsList";
 import SubjectsCreate from "./pages/subjects/SubjectsCreate";
+import ClassList from "./pages/classes/ClassList";
+import ClassCreate from "./pages/classes/ClassCreate";
 
 function App() {
   return (
@@ -43,9 +45,17 @@ function App() {
                   name: "subjects",
                   list: "/subjects",
                   create: "/subjects/create",
-                  edit: "/subjects/edit/:id",
-                  show: "/subjects/show/:id",
+                  // edit: "/subjects/edit/:id",
+                  // show: "/subjects/show/:id",
                   meta: { label: "Subjects", icon: <BookOpen /> },
+                },
+                {
+                  name: "classes",
+                  list: "/classes",
+                  create: "/classes/create",
+                  // edit: "/classes/edit/:id",
+                  // show: "/classes/show/:id",
+                  meta: { label: "Classes", icon: <GraduationCap /> },
                 },
               ]}
             >
@@ -61,6 +71,10 @@ function App() {
                   <Route path="/subjects">
                     <Route index element={<SubjectsList />} />
                     <Route path="create" element={<SubjectsCreate />} />
+                  </Route>
+                  <Route path="/classes">
+                    <Route index element={<ClassList />} />
+                    <Route path="create" element={<ClassCreate />} />
                   </Route>
                 </Route>
               </Routes>
